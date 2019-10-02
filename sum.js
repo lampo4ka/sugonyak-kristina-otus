870ee92d45f959b7d8baad441d7ecd5342378e52
Kristina <30chris02@gmail.com>
2019-10-02 21:55:02 +0300

    Add function sum

---
+++ sum.js
@@ -0,0 +1,12 @@
+var sum = function add(x) {
+		var count = 0;
+		function function(x) {
+				if (x === undefined) {
+						return count;
+				} else {
+						count = count + x;
+						return function;
+				}
+		}
+return function(x);
+}
