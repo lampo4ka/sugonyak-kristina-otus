a879d2336bfca73ddbb988fdbe0cfe03b43e203c
Kristina <30chris02@gmail.com>
2019-10-15 17:42:13 +0300

    Исправлены замечания

--- promiseReduceNewTry.js
+++ promiseReduceNewTry.js
@@ -4,7 +4,8 @@ function promiseReduce(asyncFunctions, reduce, initialValue) {
 
 	async function countPromise(index) {
 		if (index === asyncFunctions.length) {
-			console.log(result)
+			console.log(result);
+			return;
 		}
 
 		let aF = await asyncFunctions[index]();
@@ -22,3 +23,4 @@ const fn2 = () => new Promise(resolve => {
 	setTimeout(() => resolve(2), 1000)
 });
 promiseReduce([fn1, fn2], (memo, value) => memo * value, 2);
+
